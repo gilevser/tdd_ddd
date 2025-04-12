3818319f3ac8c4504cd133e77e1e143ef8ce3afa
Sergey Gilev <sincer89@mail.ru>
2025-04-12 18:15:45 +0500

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Ivan", "123456"));
+        assertEquals(2, phoneBook.add("Serg", "234567"));
+    }
 }
